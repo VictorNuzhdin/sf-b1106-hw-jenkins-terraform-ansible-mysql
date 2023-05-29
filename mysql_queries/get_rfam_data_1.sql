6bfb14e6c5923becd0bdf3041cad356bf575b89f
VictorNuzhdin <122347141+VictorNuzhdin@users.noreply.github.com>
2023-05-29 18:29:20 +0600

    Fix Tabs

--- mysql_queries/get_rfam_data_1.sql
+++ mysql_queries/get_rfam_data_1.sql
@@ -23,17 +23,17 @@ LIMIT 	1, 1;
 
 /**OUTPUT:
 
-RFAM_ACC    RFAMSEQ_ACC     PMID    	ORDER_ADDED     PUB_URL 										JOURNAL 								TITLE
-RF00507 	AY274119.3      15680415    1       		https://pubmed.ncbi.nlm.nih.gov/15680415/       Virology 2005;332:498-510.      		Programmed ribosomal frameshifting in decoding the SARS-CoV genome.
-RF00507 	AY274119.3      32743589    2       		https://pubmed.ncbi.nlm.nih.gov/32743589/       bioRxiv. 2020; [Epub ahead of print]    Cryo-electron Microscopy and Exploratory Antisense Targeting of the 28-kDa Frameshift Stimulation Element from the SARS-CoV-2 RNA Genome.
-RF03120 	AY274119.3      15220462    1       		https://pubmed.ncbi.nlm.nih.gov/15220462/       J Virol. 2004;78:7846-7851.     		The 3' cis-acting genomic replication element of the severe acute respiratory syndrome coronavirus can function in the murine coronavirus genome.
-RF03120 	AY274119.3      25736566    2       		https://pubmed.ncbi.nlm.nih.gov/25736566/       Virus Res. 2015;206:120-133.    		The structure and functions of coronavirus genomic 3' and 5' ends.
-RF03120 	AY274119.3      21378501    3       		https://pubmed.ncbi.nlm.nih.gov/21378501/       RNA Biol. 2011;8:237-248.       		RNA-RNA and RNA-protein interactions in coronavirus replication and transcription.
-RF03120 	AY274119.3      27712622    4       		https://pubmed.ncbi.nlm.nih.gov/27712622/       Adv Virus Res. 2016;96:127-163. 		Coronavirus cis-Acting RNA Elements.
-RF03120 	AY274119.3      35236777    5       		https://pubmed.ncbi.nlm.nih.gov/35236777/       RNA. 2022;28:729-741.   				Correlated sequence signatures are present within the genomic 5'UTR RNA and NSP1  protein in coronaviruses.
-RF03125 	AY274119.3      15220462    1       		https://pubmed.ncbi.nlm.nih.gov/15220462/       J Virol. 2004;78:7846-7851.     		The 3' cis-acting genomic replication element of the severe acute respiratory syndrome coronavirus can function in the murine coronavirus genome.
-RF03125 	AY274119.3      25736566    2       		https://pubmed.ncbi.nlm.nih.gov/25736566/       Virus Res. 2015;206:120-133.    		The structure and functions of coronavirus genomic 3' and 5' ends.
-RF03125 	AY274119.3      21378501    3       		https://pubmed.ncbi.nlm.nih.gov/21378501/       RNA Biol. 2011;8:237-248.       		RNA-RNA and RNA-protein interactions in coronavirus replication and transcription.
-RF03125 	AY274119.3      27712622    4       		https://pubmed.ncbi.nlm.nih.gov/27712622/       Adv Virus Res. 2016;96:127-163. 		Coronavirus cis-Acting RNA Elements.
+RFAM_ACC	RFAMSEQ_ACC     PMID		ORDER_ADDED     PUB_URL 					JOURNAL 				TITLE
+RF00507		AY274119.3      15680415    	1       	https://pubmed.ncbi.nlm.nih.gov/15680415/       Virology 2005;332:498-510.      	Programmed ribosomal frameshifting in decoding the SARS-CoV genome.
+RF00507 	AY274119.3      32743589    	2       	https://pubmed.ncbi.nlm.nih.gov/32743589/       bioRxiv. 2020; [Epub ahead of print]    Cryo-electron Microscopy and Exploratory Antisense Targeting of the 28-kDa Frameshift Stimulation Element from the SARS-CoV-2 RNA Genome.
+RF03120 	AY274119.3      15220462    	1       	https://pubmed.ncbi.nlm.nih.gov/15220462/       J Virol. 2004;78:7846-7851.     	The 3' cis-acting genomic replication element of the severe acute respiratory syndrome coronavirus can function in the murine coronavirus genome.
+RF03120 	AY274119.3      25736566    	2       	https://pubmed.ncbi.nlm.nih.gov/25736566/       Virus Res. 2015;206:120-133.    	The structure and functions of coronavirus genomic 3' and 5' ends.
+RF03120 	AY274119.3      21378501    	3       	https://pubmed.ncbi.nlm.nih.gov/21378501/       RNA Biol. 2011;8:237-248.       	RNA-RNA and RNA-protein interactions in coronavirus replication and transcription.
+RF03120 	AY274119.3      27712622    	4       	https://pubmed.ncbi.nlm.nih.gov/27712622/       Adv Virus Res. 2016;96:127-163. 	Coronavirus cis-Acting RNA Elements.
+RF03120 	AY274119.3      35236777    	5       	https://pubmed.ncbi.nlm.nih.gov/35236777/       RNA. 2022;28:729-741.   		Correlated sequence signatures are present within the genomic 5'UTR RNA and NSP1  protein in coronaviruses.
+RF03125 	AY274119.3      15220462    	1       	https://pubmed.ncbi.nlm.nih.gov/15220462/       J Virol. 2004;78:7846-7851.     	The 3' cis-acting genomic replication element of the severe acute respiratory syndrome coronavirus can function in the murine coronavirus genome.
+RF03125 	AY274119.3      25736566    	2       	https://pubmed.ncbi.nlm.nih.gov/25736566/       Virus Res. 2015;206:120-133.    	The structure and functions of coronavirus genomic 3' and 5' ends.
+RF03125 	AY274119.3      21378501    	3       	https://pubmed.ncbi.nlm.nih.gov/21378501/       RNA Biol. 2011;8:237-248.       	RNA-RNA and RNA-protein interactions in coronavirus replication and transcription.
+RF03125 	AY274119.3      27712622    	4       	https://pubmed.ncbi.nlm.nih.gov/27712622/       Adv Virus Res. 2016;96:127-163. 	Coronavirus cis-Acting RNA Elements.
 */
 -- 11 records (total)
